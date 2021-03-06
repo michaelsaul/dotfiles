d38b34b520920f9a20dfa381aae59b6ec4d49e4a
Michael Saul <michael.saul@gmail.com>
2021-03-05 17:35:35 -0800

    Added zsh aliases

--- restore.sh
+++ restore.sh
@@ -16,3 +16,6 @@ brew bundle
 # Remove .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+rm -rf $HOME/aliases.zsh
+ln -s $HOME/.dotfiles/aliases.zsh $HOME/aliases.zsh
